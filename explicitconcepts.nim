d23675fc84eaa0d62bcd5bbe37140cafb7337e21
gemath <33119724+gemath@users.noreply.github.com>
2017-10-26 18:49:01 +0200

    Update explicitconcepts.nim

--- explicitconcepts.nim
+++ explicitconcepts.nim
@@ -1,8 +1,18 @@
-#
-# Copyright (c) 2017 Gerd Mathar
-# Published under the Apache License 2.0
-# See the file "LICENSE" included in this distribution for licensing and copying details.
-#
+#[
+   Copyright 2017 Gerd Mathar
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+]#
 
 ## This module provides explicit concepts and an ``implements``-relation
 ## between concepts and implementing types.
